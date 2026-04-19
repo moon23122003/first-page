314f7417efde31f82b70ada595fda5495e880720
moon23122003 <monuvijayalaxmi@gmail.com>
2026-04-19 21:12:38 +0530

    Change link button URL in sign up page

--- home.py
+++ home.py
@@ -39,7 +39,7 @@ if page == "Home":
 # ---------------- SIGN UP ----------------
 elif page == "sign up":
    import streamlit as st
-st.link_button("https://www.hfmmagazine.com/sites/default/files/hfmmagazine/ext/resources/images/2016/October/1116_upfront_microhospital_BSW.jpg")
+st.link_button("https://www.google.com/search?sca_esv=31d988af6d9099b2&sxsrf=ANbL-n6nsHxAKpNfsj5RWm_BjcR7aYAfIw:1776612882756&udm=2&fbs=ADc_l-aN0CWEZBOHjofHoaMMDiKpaEWjvZ2Py1XXV8d8KvlI3jljrY5CkLlk8Dq3IvwBz-S-fac5VpaUJQrvk4_-zpSlpwA8AVxh0KRzghNF-5DwK1TTQDmkkuNGtaYgDgB379e7vcGqtsDaGjSx2bmixBEAL8V8S-J-9orpNCF4S_kulUdR495OT3wZlaApE9eeUB-2CRL_7OlZBelIC4xpDmesxUV_Sw&q=hospital&sa=X&ved=2ahUKEwiMvZ7envqTAxVSm68BHbr5FlwQtKgLegQIFRAB&biw=1920&bih=869&dpr=1#sv=CAMSVhoyKhBlLXFFemZBX21ubGw4WnRNMg5xRXpmQV9tbmxsOFp0TToOX21sN1FwRVVaUWh3Tk0gBCocCgZtb3NhaWMSEGUtcUV6ZkFfbW5sbDhadE0YADABGAcg6LeJhwxKCBACGAEgAigB","https://www.hfmmagazine.com/sites/default/files/hfmmagazine/ext/resources/images/2016/October/1116_upfront_microhospital_BSW.jpg")
 st.title("SingUp")
 name=st.text_input("USER NAME")
 password=st.text_input("PASSWORD")
